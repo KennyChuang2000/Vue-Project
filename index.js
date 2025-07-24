4629e3f4445da24b4efa448e1d3fb09fc923f4e8
kennychuang2000 <2135032@gmail.com>
2025-07-24 23:37:17 +0800

    Replace old project with new version

---
+++ index.js
@@ -0,0 +1,137 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            name: ["123", "456", false, 456],
+        };
+    },
+});
+app.mount("#app");
+
+const link = Vue.createApp({
+    data() {
+        return {
+            link: "https://www.youtube.com/",
+        };
+    }
+});
+link.mount("#link1");
+
+
+const todo = Vue.createApp({
+    data() {
+        return {
+            todos: ["todo1", "todo2", "todo3"],
+        }
+    }
+});
+todo.mount("#todoList");
+
+const todoCheck = Vue.createApp({
+    data() {
+        return {
+            todos: [
+                { name: "todo1", check: false },
+                { name: "todo2", check: true },
+                { name: "todo3", check: false }
+            ]
+        }
+    }
+});
+todoCheck.mount("#todoCheckList");
+
+const todoIf = Vue.createApp({
+    data() {
+        return {
+            todos: [
+                // { name: "todo1", show: false, check: false }
+            ]
+        }
+    }
+});
+todoIf.mount("#todoIfList");
+
+const onClick = Vue.createApp({
+    data() {
+        return {
+            showAnswer: false
+        }
+    }
+});
+onClick.mount("#onClick");
+
+const computed = Vue.createApp({
+    data() {
+        return {
+            showAnswer: false
+        }
+    },
+    computed: {
+        label() {
+            return this.showAnswer ? "隱藏答案" : "顯示答案";
+        }
+    },
+});
+computed.mount("#computed");
+
+const methods = Vue.createApp({
+    data() {
+        return {
+            showAnswer: false
+        }
+    },
+    computed: {
+        label() {
+            return this.showAnswer ? "隱藏答案" : "顯示答案";
+        },
+    },
+    methods: {
+        toggleShowAnswer() {
+            this.showAnswer = !this.showAnswer;
+        }
+    },
+});
+methods.mount("#methods");
+
+const watch = Vue.createApp({
+
+    data() {
+        return {
+            showAnswer: false,
+            countDown: 5,
+            timer: null,
+        }
+    },
+    computed: {
+        label() {
+            return this.showAnswer ? "隱藏答案" + this.countDown : "顯示答案";
+        },
+    },
+    methods: {
+        toggleShowAnswer() {
+            this.showAnswer = !this.showAnswer;
+        },
+    },
+    watch: {
+        showAnswer(newVal, oldVal) {
+            if (newVal) {
+                this.countDown = 5;
+                if (this.timer) {
+                    clearInterval(this.timer);
+                    this.timer = null;
+                }
+                this.timer = setInterval(() => {
+                    this.countDown--;
+                    if (this.countDown <= 0) {
+                        this.showAnswer = false;
+                        clearInterval(this.timer);
+                        this.timer = null;
+                    }
+                }, 1000);
+            } else {
+                clearInterval(this.timer);
+            }
+
+        },
+    },
+});
+watch.mount("#watch");
