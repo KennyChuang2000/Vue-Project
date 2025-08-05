1367130c00681fb3e60b586a0e582380ee7f2088
kennychuang2000 <2135032@gmail.com>
2025-08-05 23:55:38 +0800

    34. 再看 v-for 指令

--- index.js
+++ index.js
@@ -20,3 +20,48 @@ const vonce = Vue.createApp({
     },
 });
 vonce.mount("#vonce");
+
+const advance_v_for = Vue.createApp({
+    data() {
+        return {
+            blogPost: {
+                title: "Vue.js 教程",
+                author: "Vue 教程小組",
+                pubDate: "2023-10-01",
+            },
+            todos: [
+                { id: 1, text: "學習 Vue.js" },
+                { id: 2, text: "學習 Vue Router" },
+                { id: 3, text: "學習 Vuex" },
+            ]
+        }
+    },
+    methods: {
+        changeBlogPost() {
+            const todos_length = this.todos.length
+            for (let i = 0; i < todos_length; i++) {
+                this.todos[i].id += 1;
+            }
+            this.todos[todos_length - 1].id = 1;
+        }
+    },
+});
+advance_v_for.mount("#advance_v-for");
+
+
+Vue.createApp({
+    data() {
+        return {
+            items: [
+                { text: '項目 A' },
+                { text: '項目 B' },
+                { text: '項目 C' }
+            ]
+        };
+    },
+    methods: {
+        swap() {
+            this.items.reverse();
+        }
+    }
+}).mount('#test-key-diff');
