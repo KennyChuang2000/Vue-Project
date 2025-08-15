b22a78d01a41c8cc99bd12b91707267b72e5f47c
kenny.chuang <kenny.chuang@innolux.com>
2025-08-15 16:24:11 +0800

    46. 全局注册组件

--- index.js
+++ index.js
@@ -19,5 +19,15 @@ stringLengthCount.component('string-length-count', {
     }
 })
 
+stringLengthCount.component('string-length-count-word', {
+    template: `
+        <div>
+           <h2>字串長度計算</h2>
+            <string-length-count></string-length-count>
+        </div>
+    `,
+})
+
+
 const vm = stringLengthCount.mount('#stringLengthCount');
 
