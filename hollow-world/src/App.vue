8c424a3e3570428e53aab0b67d11c0c30bcdab4b
kennychuang2000 <2135032@gmail.com>
2025-08-17 21:45:45 +0800

    52. props 属性：自定义组件内容

--- hollow-world/src/App.vue
+++ hollow-world/src/App.vue
@@ -1,7 +1,9 @@
 <script >
 import WordCount from './components/WordCount.vue'
+import MessageItem from './components/MessageItem.vue'
 export default {
-  components: { WordCount }
+  components: { WordCount , MessageItem },
+  
 }
 </script>
 
@@ -11,6 +13,13 @@ export default {
   <div>
     <h1>字數統計</h1>
     <WordCount></WordCount>
+  </div>
+
+  <div>
+    <h1>留言列表</h1>
+    <MessageItem sender="Alice" time="12:00" msg="Hello, world!"></MessageItem>
+    <MessageItem sender="Bob" time="12:05" msg="Vue.js is awesome!"></MessageItem>
+    <MessageItem sender="Charlie" time="12:10" msg="Let's build something great!"></MessageItem>
   </div>
 </template>
 
---
+++ hollow-world/src/components/MessageItem.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <p> {{sender}} </p> 
+       <span> {{time}} </span>
+        <p> {{msg}} </p>
+    </div>
+</template>
+
+<script>
+export default {
+   props:[
+        'sender',
+        'time',
+        'msg'
+    ],
+};
+</script>
+
+<style scoped>
+div{
+display:grid;
+grid-template-areas: "sender time"
+                     "msg msg";
+border: 1px solid #e91919;
+border-radius:  4px;
+padding:  1em;
+width: 250px;
+}
+   
+
+
+</style>
