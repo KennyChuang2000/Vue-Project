6abeed356a97feddc5c7305095fe0838f2f45f35
kennychuang2000 <2135032@gmail.com>
2025-08-17 21:55:56 +0800

    53. 组件动态属性值绑定

--- hollow-world/src/App.vue
+++ hollow-world/src/App.vue
@@ -1,10 +1,19 @@
-<script >
+<script>
 import WordCount from './components/WordCount.vue'
 import MessageItem from './components/MessageItem.vue'
 export default {
-  components: { WordCount , MessageItem },
-  
+  components: { WordCount, MessageItem },
+  data() {
+    return {
+      messages: [
+        { id: 1, sender: 'Alice', time: '12:00', msg: 'Hello, worldxx!' },
+        { id: 2, sender: 'Bob', time: '12:05', msg: 'Vue.js is awesomexx!' },
+        { id: 3, sender: 'Charlie', time: '12:10', msg: 'Let\'s build something greatxxxx!' },
+      ],
+    }
+  },
 }
+
 </script>
 
 
@@ -21,6 +30,14 @@ export default {
     <MessageItem sender="Bob" time="12:05" msg="Vue.js is awesome!"></MessageItem>
     <MessageItem sender="Charlie" time="12:10" msg="Let's build something great!"></MessageItem>
   </div>
+
+  <div>
+    <h1>留言列表抓 app.vue 的data(){} 資料</h1>
+    <MessageItem v-for="message in messages" :key="message.id" :sender="message.sender" :time="message.time"
+      :msg="message.msg"></MessageItem>
+    <h3>v-bind="message" 等價 :sender="message.sender" :time="message.time" :msg="message.msg"</h3>
+    <MessageItem v-for="message in messages" :key="message.id" v-bind="message"></MessageItem>
+  </div>
 </template>
 
 <style scoped>
@@ -30,9 +47,11 @@ export default {
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
